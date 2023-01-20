2c162eae6ff12f7b996e9bbc9282348bf3418d89
notsoloud <51118069+notsoloud@users.noreply.github.com>
2023-01-20 12:19:57 +0530

    Update F1.java

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,7 +4,7 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-        //Which constructor should call ?
+        //Which constructor should ?
 
         super(name,4,2,7,isManual,"Sports",2);
 
